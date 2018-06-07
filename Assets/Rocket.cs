f39e5d2aafa4d4acaa51db3280db6c94133e1881
nicklasoxhammar <nicklas.oxhammar@hotmail.com>
2018-06-07 14:46:36 +0200

    Improved movement, destroy rocket ship on non friendlycollison, fades sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,44 +5,97 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour {
 
+    [SerializeField]
+    float rcsThrust = 100f;
+
+    [SerializeField]
+    float mainThrust = 20f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+    float rocketSoundVolume;
 
 	// Use this for initialization
 	void Start () {
 
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        rocketSoundVolume = audioSource.volume;
+        
 
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-		
-	}
+        Rotate();
+        Thrust();
 
-    private void ProcessInput() {
-        
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+
+        if(collision.gameObject.tag != "Friendly") {
+            DestroyShip();
+        }
+    }
+
+
+    private void Rotate() {
+
+        rigidBody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false;
+
+    }
+
+    private void Thrust() {
+
         if (Input.GetKey(KeyCode.Space)) {
 
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
 
             if (!audioSource.isPlaying) {
+                audioSource.volume = rocketSoundVolume;
                 audioSource.Play();
             }
         }
-        else {
-            audioSource.Stop();
+       
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            StartCoroutine(VolumeFade(audioSource, 0f, 0.5f));
         }
+    }
 
-
-        if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward);
-        }else if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(-Vector3.forward);
-        }
+    void DestroyShip() {
+        Destroy(this.gameObject);
 
     }
+
+    IEnumerator VolumeFade(AudioSource _AudioSource, float _EndVolume, float _FadeLength) {
+
+        float _StartVolume = _AudioSource.volume;
+
+        float _StartTime = Time.time;
+
+        while (Time.time < _StartTime + _FadeLength) {
+
+            _AudioSource.volume = _StartVolume + ((_EndVolume - _StartVolume) * ((Time.time - _StartTime) / _FadeLength));
+
+            yield return null;
+
+        }
+
+        if (_EndVolume == 0) { _AudioSource.Stop(); }
+
+    }
+
 }
